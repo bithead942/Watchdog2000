d307ebe4a90e5a9c8229de4eb4770aca34c61a96
Bithead942 <bithead942@gmail.com>
2024-09-12 14:25:06 -0400

    Add files via upload

---
+++ WebIrrigationControl/WebIrrigationControl/default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="WebIrrigationControl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Watchdog Irrigation Control</title>
+    <style type="text/css">
+        .auto-style1
+        {
+            width: 68px;
+        }
+        </style>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div style="text-align:center">
+        <table style="width:100%;">
+            <tr>
+                <td class="auto-style1">
+            <img alt="" src="Watchdog2.ico" style="height: 58px; width: 60px" /></td>
+                <td><h1>Watchdog Irrigation Control</h1></td>
+            </tr>
+        </table>
+    </div>
+        
+    <h3>Current Status:  <asp:Label runat="server" ID="lblCurrentStatus" /></h3>
+         <asp:ImageMap ID="ImageMap1" runat="server" HotSpotMode="PostBack" ImageUrl="~/Irrigation Lines3.GIF" ImageAlign="Top">
+             <asp:PolygonHotSpot AlternateText="Zone 1" Coordinates="595,262,654,262,654,370,595,370" HotSpotMode="PostBack" PostBackValue="1" />
+             <asp:PolygonHotSpot AlternateText="Zone 2" Coordinates="314,452,469,385,668,377,596,502,307,511" HotSpotMode="PostBack" PostBackValue="2"/>
+             <asp:PolygonHotSpot AlternateText="Zone 3" Coordinates="658,357,660,303,785,293,900,361,780,470,753,472" HotSpotMode="PostBack" PostBackValue="3" />
+             <asp:PolygonHotSpot AlternateText="Zone 4" Coordinates="359,533,551,511,747,495,749,535,620,570,380,585" HotSpotMode="PostBack" PostBackValue="4" />
+             <asp:PolygonHotSpot AlternateText="Zone 5" Coordinates="284,586,671,586,671,637,284,637" HotSpotMode="PostBack" PostBackValue="5" />
+             <asp:PolygonHotSpot AlternateText="Zone 6" Coordinates="708,561,888,461,1017,309,1063,333,935,505,720,609" HotSpotMode="PostBack" PostBackValue="6" />
+             <asp:PolygonHotSpot AlternateText="Zone 7" Coordinates="646,32,988,245,957,313,608,96" HotSpotMode="PostBack" PostBackValue="7" />
+             <asp:PolygonHotSpot AlternateText="Zone 8" Coordinates="414,194,548,173,571,194,540,262,430,261" HotSpotMode="PostBack" PostBackValue="8" />
+             <asp:PolygonHotSpot AlternateText="Zone 9" Coordinates="431,163,428,102,562,93,744,215,721,256,547,146" HotSpotMode="PostBack" PostBackValue="9" />
+             <asp:PolygonHotSpot AlternateText="Zone 10" Coordinates="520,0,580,20,105,256,89,192" HotSpotMode="PostBack" PostBackValue="10" />
+             <asp:PolygonHotSpot AlternateText="Zone 11" Coordinates="'246,446,311,305,430,239,432,387,341,387,289,470'" HotSpotMode="PostBack" PostBackValue="11" />
+             <asp:PolygonHotSpot AlternateText="Zone 12" Coordinates="342,151,391,213,187,381,130,309" HotSpotMode="PostBack" PostBackValue="12" />
+             <asp:PolygonHotSpot AlternateText="Zone 13" Coordinates="0,260,52,240,238,526,193,555,62,437" HotSpotMode="PostBack" PostBackValue="13" />
+         </asp:ImageMap>
+
+
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:WatchdogConnectionString1 %>">
+        </asp:SqlDataSource>    
+    </form>
+    <meta http-equiv="refresh" content="15"/>
+</body>
+</html>
---
+++ WebIrrigationControl/WebIrrigationControl/default.aspx.vb
@@ -0,0 +1,76 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+    Dim ZoneState(15) As Integer
+    Dim dtStartTime As DateTime
+    Dim bRunning As Boolean
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim iDurationMin, iDurationSec As Integer
+
+        GetCurrentStatus()
+        lblCurrentStatus.Text = ""
+
+        iDurationMin = DateDiff(DateInterval.Second, dtStartTime, Now()) \ 60
+        iDurationSec = DateDiff(DateInterval.Second, dtStartTime, Now()) Mod 60
+
+        For i = 1 To 14
+            If ZoneState(i) = 1 Then
+                lblCurrentStatus.Text = "Zone " & i.ToString & " Active.  [Duration: "
+                If iDurationMin < 10 Then
+                    lblCurrentStatus.Text = lblCurrentStatus.Text & "0" & iDurationMin.ToString & ":"
+                Else
+                    lblCurrentStatus.Text = lblCurrentStatus.Text & iDurationMin.ToString & ":"
+                End If
+                If iDurationSec < 10 Then
+                    lblCurrentStatus.Text = lblCurrentStatus.Text & "0" & iDurationSec.ToString & "]"
+                Else
+                    lblCurrentStatus.Text = lblCurrentStatus.Text & iDurationSec.ToString & "]"
+                End If
+            End If
+        Next
+        If lblCurrentStatus.Text = "" Then
+            lblCurrentStatus.Text = "System Off"
+        End If
+
+    End Sub
+
+    Protected Sub GetCurrentStatus()
+        Dim dv As DataView
+        Dim i As Integer
+
+        SqlDataSource1.SelectCommandType = SqlDataSourceCommandType.Text
+        SqlDataSource1.SelectCommand() = "SELECT Zone_ID, Zone_Current_State FROM Irrigation_Control ORDER BY Zone_ID ASC"
+        dv = CType(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+        For i = 0 To dv.Table.Rows.Count - 1
+            ZoneState(i) = CType(dv.Table.Rows(i)(1), Integer)
+            If ZoneState(i) = 1 And i > 0 Then
+                bRunning = True
+            End If
+        Next
+        If bRunning Then
+            dv = Nothing
+            SqlDataSource1.SelectCommandType = SqlDataSourceCommandType.Text
+            SqlDataSource1.SelectCommand() = "SELECT Event_Timestamp FROM Irrigation_History WHERE Event_ID IN ('201', '203', '205', '207', '209', '211', '213', '215', '217', '219', '221', '223', '225') ORDER BY Event_Timestamp DESC"
+            dv = CType(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+            If dv.Table.Rows.Count > 0 Then
+                dtStartTime = CType(dv.Table.Rows(0)(0), DateTime)
+            Else
+                dtStartTime = Nothing
+            End If
+        Else
+            dtStartTime = Nothing
+        End If
+    End Sub
+
+    Private Sub ImageMap1_Click1(sender As Object, e As ImageMapEventArgs) Handles ImageMap1.Click
+        GetCurrentStatus()
+
+        If ZoneState(CInt(e.PostBackValue)) = 1 Then
+            SqlDataSource1.UpdateCommand() = "Update Irrigation_Control SET Zone_Request_State = 0 WHERE ZONE_ID = " & e.PostBackValue
+            SqlDataSource1.Update()
+        ElseIf ZoneState(CInt(e.PostBackValue)) = 0 Then
+            SqlDataSource1.UpdateCommand() = "Update Irrigation_Control SET Zone_Request_State = 1 WHERE ZONE_ID = " & e.PostBackValue
+            SqlDataSource1.Update()
+        End If
+    End Sub
+End Class
